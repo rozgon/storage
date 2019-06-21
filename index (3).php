411f67b1b4bceb224595b3e6410843f0c7becdf5
rozgon <52061150+rozgon@users.noreply.github.com>
2019-06-21 11:51:55 +0600

    Add files via upload

---
+++ index (3).php
@@ -0,0 +1,11 @@
+[Process]
+Type=Edit file
+Engine=MediaWiki
+Script=https://wf.mail.ru/wiki/index.php
+Server=https://wf.mail.ru
+Path=/wiki
+Special namespace=Служебная
+
+[File]
+Extension=png
+URL=https://wf.cdn.gmru.net/wiki/images/0/0f/Image_icon_allowed_to_tournaments.png
